7fd972bf9b79bf3e6d2aaee871a0da5489cfb8f4
WFayoz <WFayoz@yahoo.com>
2024-05-10 12:36:52 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-// App.jsx
 import React, { useState } from "react";
 import OrderSection from "./components/order/ OrderSection.jsx";
 import Sidebar from "./components/sidebar/Sidebar.jsx";
 import HomePage from "./pages/HomePage.jsx";
+import { FaRegCreditCard } from "react-icons/fa6";
+import { SlPaypal } from "react-icons/sl";
+import { LuWallet } from "react-icons/lu";
+import { FaCheckCircle } from "react-icons/fa";
+import './App.css'
 
 const App = () => {
   const [selectedCards, setSelectedCards] = useState([]);
   const [selectedOption, setSelectedOption] = useState("dine-in");
+  const [page, setPage] = useState(false);
+  const [active, setActive] = useState(null);
 
   const handleCardClick = (card) => {
     const existingCardIndex = selectedCards.findIndex(
@@ -32,17 +38,206 @@ const App = () => {
   };
 
   return (
-    <div className="w-[full] bg-[#252836] flex">
-      <Sidebar />
-      <HomePage
-        onCardClick={handleCardClick}
-        onSelectOption={handleSelectOption}
-      />
-      <OrderSection
-        selectedCards={selectedCards}
-        onRemoveCard={handleRemoveCard}
-        selectedOption={selectedOption}
-      />
+    <div>
+      <div className="w-[full] bg-[#252836] flex">
+        <Sidebar />
+        <HomePage
+          onCardClick={handleCardClick}
+          onSelectOption={handleSelectOption}
+        />
+        <OrderSection
+          setPage={setPage}
+          selectedCards={selectedCards}
+          onRemoveCard={handleRemoveCard}
+          selectedOption={selectedOption}
+        />
+      </div>
+      {page ? (
+        <div id="payContainer">
+          <div className="payment">
+            <div className="payment__wrapper">
+              <div className="paymentTitle">
+                <h1 className="paymentTitlePay">
+                  Payment
+                </h1>
+                <p className="payMet">
+                  3 payment method available
+                </p>
+              </div>
+              <div>
+                <h1 className="text-white text-[24px] font-semibold">
+                  Payment Method
+                </h1>
+                <div className="flex pt-6 gap-4 cursor-pointer">
+                  <div
+                    className={
+                      active == 1
+                        ? "  h-16 rounded-lg min-w-28 border-white border-4 "
+                        : "border  px-3 py-2  h-16 rounded-lg min-w-28  "
+                    }
+                    onClick={() => setActive(1)}
+                  >
+                    <div className="flex mb-2">
+                      <FaRegCreditCard
+                        className={
+                          active == 1
+                            ? "w-6 h-6 text-white block mx-auto"
+                            : "w-6 h-6  text-[#ABBBC2] block mx-auto"
+                        }
+                      />
+                      {active == 1 ? (
+                        <FaCheckCircle className="text-[#EA7C69]" />
+                      ) : (
+                        <></>
+                      )}
+                    </div>
+                    <h1
+                      className={active == 1 ? "text-white" : "text-[#ABBBC2]"}
+                    >
+                      Credit Card
+                    </h1>
+                  </div>
+                  <div
+                    className={
+                      active == 2
+                        ? "  rounded-lg h-16 min-w-28 border-white border-4 cursor-pointer "
+                        : "border px-3 py-2 h-16 rounded-lg min-w-28   cursor-pointer"
+                    }
+                    onClick={() => setActive(2)}
+                  >
+                    <div className="flex mb-2">
+                      <SlPaypal
+                        className={
+                          active == 2
+                            ? "w-6 h-6 text-white block mx-auto"
+                            : "w-6 h-6  text-[#ABBBC2] block mx-auto"
+                        }
+                      />
+                      {active == 2 ? (
+                        <FaCheckCircle className="text-[#EA7C69]" />
+                      ) : (
+                        <></>
+                      )}
+                    </div>
+                    <h1
+                      className={active == 2 ? "text-white" : "text-[#ABBBC2]"}
+                    >
+                      Credit Card
+                    </h1>
+                  </div>
+                  <div
+                    className={
+                      active == 3
+                        ? "  h-16 rounded-lg min-w-28 border-white border-4 cursor-pointer "
+                        : "border px-3 py-2  h-16 rounded-lg min-w-28   cursor-pointer"
+                    }
+                    onClick={() => setActive(3)}
+                  >
+                    <div className="flex mb-2">
+                      <LuWallet
+                        className={
+                          active == 3
+                            ? "w-6 h-6 text-white block mx-auto"
+                            : "w-6 h-6  text-[#ABBBC2] block mx-auto"
+                        }
+                      />
+                      {active == 3 ? (
+                        <FaCheckCircle className="text-[#EA7C69]" />
+                      ) : (
+                        <></>
+                      )}
+                    </div>
+                    <h1
+                      className={active == 3 ? "text-white" : "text-[#ABBBC2]"}
+                    >
+                      Credit Card
+                    </h1>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <form className=" w-full pt-6">
+                  <label className="text-white font-medium mt-5">
+                    Cardholder Name
+                  </label>
+                  <div className="w-full mt-2  mb-3">
+                    <input
+                      type="text "
+                      placeholder="Levi Ackerman"
+                      className=" w-full  py-3 pl-3 bg-[#393C49] rounded-lg"
+                    />
+                  </div>
+
+                  <label className="text-white font-medium ">Card Number</label>
+                  <div>
+                    <input
+                      className="w-full py-3 pl-3 bg-[#393C49] rounded-lg"
+                      type="text "
+                      placeholder="2564 1421 0897 1244"
+                    />
+                  </div>
+                  <div className="flex justify-between">
+                    <div>
+                      <p className="text-white font-medium mt-5 mb-2">
+                        Expiration Date
+                      </p>
+                      <input
+                        type="data"
+                        placeholder="02/2022"
+                        className="w-[180px] py-3 pl-3 bg-[#393C49] rounded-lg"
+                      />
+                    </div>
+                    <div>
+                      <p className="text-white font-medium mt-5 mb-2">CVV</p>
+                      <input
+                        type="text "
+                        placeholder="...."
+                        className="w-[180px] py-3 pl-3 bg-[#393C49] rounded-lg mb-3"
+                      />
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="flex justify-between">
+                    <div>
+                      <p className="text-white font-medium mt-5 mb-2">
+                        Order Type
+                      </p>
+                      <input
+                        type="text "
+                        placeholder="Dine In"
+                        className="w-[180px] py-3 pl-3 bg-[#393C49] rounded-lg"
+                      />
+                    </div>
+                    <div>
+                      <p className="text-white font-medium mt-5 mb-2">
+                        Table no.
+                      </p>
+                      <input
+                        type="text "
+                        placeholder="140"
+                        className="w-[180px] py-3 pl-3 bg-[#393C49] rounded-lg mb-3"
+                      />
+                    </div>
+                  </div>
+                </form>
+                <div className="flex justify-between mt-5">
+                  <button
+                    className="w-[180px] py-3 border text-[#EA7C69] rounded-lg border-[#EA7C69]"
+                    onClick={() => setPage(false)}
+                  >
+                    Cancel
+                  </button>
+                  <button className="w-[180px] py-3 border border-[#EA7C69] text-[white] rounded-lg bg-[#EA7C69]">
+                    Confirm Payment
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
--- src/components/order/ OrderSection.jsx
+++ src/components/order/ OrderSection.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import "./order.scss";
 import { LuTrash } from "react-icons/lu";
 
-const OrderSection = ({ selectedCards, onRemoveCard }) => {
+const OrderSection = ({ selectedCards, onRemoveCard , setPage}) => {
   const [focusedItem, setFocusedItem] = useState(1);
+  
 
   const handleItemClick = (index) => {
     if (focusedItem === index) {
@@ -17,7 +18,6 @@ const OrderSection = ({ selectedCards, onRemoveCard }) => {
     onRemoveCard(index);
   };
 
-
   const calculateOverallSum = () => {
     return selectedCards.reduce((acc, card) => {
       return acc + card.sum * card.quantity;
@@ -25,85 +25,85 @@ const OrderSection = ({ selectedCards, onRemoveCard }) => {
   };
 
   return (
-    <div className="order">
-      <div className="order__title">
-        <h1>Orders #34562</h1>
-        <ul>
-          <li
-            className={`order__li ${focusedItem === 1 ? "active" : ""}`}
-            onClick={() => handleItemClick(1)}
-          >
-            Dine In
-          </li>
-          <li
-            className={`order__li ${focusedItem === 2 ? "active" : ""}`}
-            onClick={() => handleItemClick(2)}
-          >
-            To Go
-          </li>
-          <li
-            className={`order__li ${focusedItem === 3 ? "active" : ""}`}
-            onClick={() => handleItemClick(3)}
-          >
-            Delivery
-          </li>
-        </ul>
-      </div>
-      <div className="order__shce">
-        <p>Item</p>
-        <div>
-          <p>Qty</p>
-          <p>Price</p>
+      <div className="order">
+        <div className="order__title">
+          <h1>Orders #34562</h1>
+          <ul>
+            <li
+              className={`order__li ${focusedItem === 1 ? "active" : ""}`}
+              onClick={() => handleItemClick(1)}
+            >
+              Dine In
+            </li>
+            <li
+              className={`order__li ${focusedItem === 2 ? "active" : ""}`}
+              onClick={() => handleItemClick(2)}
+            >
+              To Go
+            </li>
+            <li
+              className={`order__li ${focusedItem === 3 ? "active" : ""}`}
+              onClick={() => handleItemClick(3)}
+            >
+              Delivery
+            </li>
+          </ul>
         </div>
-      </div>
-      <div className="order__taken">
-        {selectedCards.map((card, index) => (
-          <div key={index} className="order__choose">
-            <div className="order__choose--top">
-              <div className="order__choose--top__left">
-                <img src={card.img} alt="" />
-                <div>
-                  <h3>{card.title}</h3>
-                  <p>${card.sum}</p>
+        <div className="order__shce">
+          <p>Item</p>
+          <div>
+            <p>Qty</p>
+            <p>Price</p>
+          </div>
+        </div>
+        <div className="order__taken">
+          {selectedCards.map((card, index) => (
+            <div key={index} className="order__choose">
+              <div className="order__choose--top">
+                <div className="order__choose--top__left">
+                  <img src={card.img} alt="" />
+                  <div>
+                    <h3>{card.title}</h3>
+                    <p>${card.sum}</p>
+                  </div>
+                </div>
+                <div className="order__choose--top__right">
+                  <div className="order__choose--top__right--qnt">
+                    {card.quantity}
+                  </div>
+                  <div className="order__choose--top__right--overall">
+                    ${card.sum * card.quantity}
+                  </div>
                 </div>
               </div>
-              <div className="order__choose--top__right">
-                <div className="order__choose--top__right--qnt">
-                  {card.quantity}
+              <div className="order__choose--bottom">
+                <div className="order__choose--bottom__left">
+                  <input type="text" placeholder="Order Note..." />
                 </div>
-                <div className="order__choose--top__right--overall">
-                  ${card.sum * card.quantity}
+                <div
+                  className="order__choose--bottom__right"
+                  onClick={() => handleRemoveCard(index)}
+                >
+                  <LuTrash className="trashCan" />
                 </div>
               </div>
             </div>
-            <div className="order__choose--bottom">
-              <div className="order__choose--bottom__left">
-                <input type="text" placeholder="Order Note..." />
-              </div>
-              <div
-                className="order__choose--bottom__right"
-                onClick={() => handleRemoveCard(index)}
-              >
-                <LuTrash className="trashCan" />
-              </div>
+          ))}
+        </div>
+        <div className="order__submit">
+          <div className="order__submit--price">
+            <div className="order__submit--price__overall">
+              <p>Discount</p>
+              <h6>$0</h6>
+            </div>
+            <div className="order__submit--price__overall">
+              <p>Sub total</p>
+              <h6>${calculateOverallSum()}</h6>
             </div>
           </div>
-        ))}
-      </div>
-      <div className="order__submit">
-        <div className="order__submit--price">
-          <div className="order__submit--price__overall">
-            <p>Discount</p>
-            <h6>$0</h6>
-          </div>
-          <div className="order__submit--price__overall">
-            <p>Sub total</p>
-            <h6>${calculateOverallSum()}</h6>
-          </div>
+          <button onClick={()=>setPage(true)}>Continue to Payment</button>
         </div>
-        <button>Continue to Payment</button>
       </div>
-    </div>
   );
 };
 
